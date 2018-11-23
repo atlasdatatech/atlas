79030da9baf59133356b772508b5baf78961bed9
exlimit@qq.com <exlimit@qq.com>
2018-11-24 01:45:16 +0800

    jwt enable

--- atlas.go
+++ atlas.go
@@ -107,8 +107,8 @@ func bindRoutes(r *gin.Engine) {
 
 	//users
 	user := r.Group("/users")
-	// user.Use(authMid.MiddlewareFunc())
-	// user.Use(NewAuthorizer(casEnf))
+	user.Use(authMid.MiddlewareFunc())
+	user.Use(NewAuthorizer(casEnf))
 	{
 		//authn > users
 		user.GET("/", listUsers)
@@ -129,8 +129,8 @@ func bindRoutes(r *gin.Engine) {
 	}
 	//roles
 	role := r.Group("/roles")
-	// role.Use(authMid.MiddlewareFunc())
-	// role.Use(NewAuthorizer(casEnf))
+	role.Use(authMid.MiddlewareFunc())
+	role.Use(NewAuthorizer(casEnf))
 	{
 		//authn > roles
 		role.GET("/", listRoles)
@@ -145,7 +145,7 @@ func bindRoutes(r *gin.Engine) {
 	}
 	//account
 	account := r.Group("/account")
-	// account.Use(authMid.MiddlewareFunc())
+	account.Use(authMid.MiddlewareFunc())
 	{
 		account.GET("/index/", renderAccount)
 		account.GET("/", getUser)
@@ -158,7 +158,7 @@ func bindRoutes(r *gin.Engine) {
 	}
 	//maproute
 	maproute := r.Group("/maps")
-	// maproute.Use(authMid.MiddlewareFunc())
+	maproute.Use(authMid.MiddlewareFunc())
 	{
 		// > map op
 		maproute.GET("/", listMaps)
@@ -167,6 +167,7 @@ func bindRoutes(r *gin.Engine) {
 		maproute.POST("/:id/", updInsetMap)
 		maproute.POST("/:id/del/", deleteMap)
 	}
+
 	//studio
 	studio := r.Group("/studio")
 	// studio.Use(authMid.MiddlewareFunc())
