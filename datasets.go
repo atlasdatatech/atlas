62572c5ebc0d645a0f4ed05133e214806e9b0c70
atlasdatatech <atlasdatatech@qq.com>
2019-01-17 10:00:02 +0800

    csv xy坐标识别

--- datasets.go
+++ datasets.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/atlasdatatech/chardet"
+	"github.com/axgle/mahonia"
 	"github.com/jinzhu/gorm"
+	"github.com/kniren/gota/dataframe"
+	"github.com/kniren/gota/series"
 
 	_ "github.com/mattn/go-sqlite3" // import sqlite3 driver
 	log "github.com/sirupsen/logrus"
@@ -56,7 +62,7 @@ func (dtfile *Datafile) upInsert() error {
 }
 
 //getEncoding guess data file encoding
-func (dtfile *Datafile) getEncoding() (string, error) {
+func (dtfile *Datafile) detechEncoding() (string, error) {
 	if dtfile == nil {
 		return "", fmt.Errorf("dtfile may not be nil")
 	}
@@ -65,7 +71,52 @@ func (dtfile *Datafile) getEncoding() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf(`data file read failed, details: %s`, err)
 	}
-	return chardet.Mostlike(buf), nil
+	en := chardet.Mostlike(buf)
+	if dtfile.Encoding == "" {
+		dtfile.Encoding = en
+	}
+	return en, nil
+}
+
+//getDataFrame get data preview context
+func (dtfile *Datafile) getDataFrame() (dataframe.DataFrame, error) {
+	empty := dataframe.New()
+	if dtfile == nil {
+		return empty, fmt.Errorf("dtfile may not be nil")
+	}
+
+	if dtfile.Encoding == "" {
+		dtfile.detechEncoding()
+	}
+
+	if dtfile.Encoding != "utf-8" {
+		buf, err := ioutil.ReadFile(dtfile.Path)
+		// ioutil.WriteFile("d:/gbk.csv", buf, os.ModePerm)
+		if err != nil {
+			log.Errorf(`read data file error, details:%s`, err)
+			return empty, err
+		}
+		//converts gbk to utf-8.
+		decode := mahonia.NewDecoder(dtfile.Encoding)
+		if decode == nil {
+			log.Errorf(`getDataFrame, mahonia new decoder error, data file encoding:%s`, dtfile.Encoding)
+			return empty, fmt.Errorf(`getDataFrame, mahonia new decoder error, data file encoding:%s`, dtfile.Encoding)
+		}
+		_, data, err := decode.Translate(buf, true)
+		if err != nil {
+			log.Errorf(`getDataFrame, mahonia decode translate error, details:%s`, err)
+		}
+		// ioutil.WriteFile("d:/utf-8.csv", data, os.ModePerm)
+		return dataframe.ReadCSV(bytes.NewReader(data)), nil
+	}
+
+	irFile, err := os.Open(dtfile.Path)
+	if err != nil {
+		log.Errorf(`read data file error, details:%s`, err)
+		return empty, err
+	}
+	defer irFile.Close()
+	return dataframe.ReadCSV(irFile), nil
 }
 
 //getPreview get data preview context
@@ -89,6 +140,62 @@ func (dtfile *Datafile) getPreview() (string, error) {
 		return "", err
 	}
 	return "", nil
+}
+
+//getPreview get data preview context
+func (dtfile *Datafile) getXYColumn() string {
+	if dtfile == nil {
+		log.Errorf("dtfile may not be nil")
+		return ""
+	}
+
+	df, err := dtfile.getDataFrame()
+	if err != nil {
+		log.Errorf(`getXYColumn, get dataframe error, details:%s`, err)
+		return ""
+	}
+
+	xcols := []string{"x", "lon", "longitude", "经度"}
+	ycols := []string{"y", "lat", "latitude", "纬度"}
+
+	getColumn := func(cols []string, names []string) string {
+		for _, c := range cols {
+			for _, n := range names {
+				if c == strings.ToLower(n) {
+					return n
+				}
+			}
+		}
+		return ""
+	}
+
+	detechColumn := func(min float64, max float64) string {
+		types := df.Types()
+		names := df.Names()
+		for i, t := range types {
+			if t == series.Float {
+				ds := df.Select([]string{names[i]}).Describe().Subset([]int{2, 6})
+				emin := ds.Elem(0, 1).Float()
+				emax := ds.Elem(0, 1).Float()
+				if emin > min && emax < max {
+					return names[i]
+				}
+			}
+		}
+		return ""
+	}
+
+	names := df.Names()
+	x := getColumn(xcols, names)
+	if x == "" {
+		x = detechColumn(73, 135)
+	}
+	y := getColumn(ycols, names)
+	if y == "" {
+		y = detechColumn(18, 54)
+	}
+
+	return x + "," + y
 }
 
 // Field represents an mbtiles file connection.
