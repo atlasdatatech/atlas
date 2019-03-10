4373fe4a042e62d2bf5fc1d90ca850d0fc5c6b78
atlasdatatech <atlasdatatech@qq.com>
2019-03-11 01:09:49 +0800

    append file

---
+++ handle_task.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"io"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func listTasks(c *gin.Context) {
+	res := NewRes()
+	// user := c.Param("user")
+	id := c.Param("id")
+	task, ok := taskSet.Load(id)
+	if ok {
+		res.DoneData(c, task)
+		return
+	}
+	dbtask := &Task{ID: id}
+	err := dbtask.info()
+	if err != nil {
+		res.FailMsg(c, "task not found")
+		return
+	}
+	res.DoneData(c, dbtask)
+}
+
+func taskQuery(c *gin.Context) {
+	res := NewRes()
+	// user := c.Param("user")
+	id := c.Param("id")
+	task, ok := taskSet.Load(id)
+	if ok {
+		res.DoneData(c, task)
+		return
+	}
+	dbtask := &Task{ID: id}
+	err := dbtask.info()
+	if err != nil {
+		res.FailMsg(c, "task not found")
+		return
+	}
+	res.DoneData(c, dbtask)
+}
+
+func taskStreamQuery(c *gin.Context) {
+	id := c.Param("id")
+	task, ok := taskSet.Load(id)
+	if ok {
+		// listener := openListener(roomid)
+		ticker := time.NewTicker(1 * time.Second)
+		// users.Add("connected", 1)
+		defer func() {
+			// closeListener(roomid, listener)
+			ticker.Stop()
+			// users.Add("disconnected", 1)
+		}()
+
+		c.Stream(func(w io.Writer) bool {
+			select {
+			// case msg := <-listener:
+			// 	messages.Add("outbound", 1)
+			// 	c.SSEvent("message", msg)
+			case <-ticker.C:
+				c.SSEvent("task", task)
+			}
+			return true
+		})
+	}
+}
